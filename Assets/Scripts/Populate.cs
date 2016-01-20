fd53f5536e004b156aae89ddf29ec8913e3cbaea
0x16e8bea <0x16e8bea@gmail.com>
2016-01-20 02:06:26 +0100

    Initial commit Added planet prefab Added 'populate'-script

---
+++ Assets/Scripts/Populate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Populate : MonoBehaviour {
+
+    public GameObject sun;
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < 3; i++)
+        {
+            Instantiate(sun, new Vector3(2 * i, 0, 0), Quaternion.identity);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
